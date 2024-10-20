abc0f781e48b4131b2ba4a12b1e2902a6aeb4d30
riya Chauhan <riyanshi49@gmail.com>
2024-10-20 14:56:45 -0400

    Commit#2: Created Unit tests for getLength and getWidth

--- RectangleTests/RectangleTests.cpp
+++ RectangleTests/RectangleTests.cpp
@@ -31,5 +31,48 @@ namespace RectangleTests
 			int results = getArea(&length, &width);
 			Assert::AreEqual(expectedArea, results);
 		}
+		// Test cases
+
+		TEST_METHOD(TestSetLength_ValidInput)
+		{
+			int length = 0;
+			setLength(40, &length);
+			Assert::AreEqual(40, length); // Normal test case 
+		}
+
+		TEST_METHOD(TestSetLength_MinimumValidInput)
+		{
+			int length = 0;
+			setLength(1, &length);
+			Assert::AreEqual(1, length); // Test Lower edge case
+		}
+		TEST_METHOD(TestSetLength_InvalidInput)
+		{
+			int length = 0;
+			setLength(100, &length);
+			Assert::AreEqual(0, length); //Invalid input test case
+		}
+
+		TEST_METHOD(TestSetWidth_ValidInput)
+		{
+			int width = 0;
+			setWidth(75, &width);
+			Assert::AreEqual(0, width); //Normal test case
+		}
+
+		TEST_METHOD(TestSetWidth_NegativeInput)
+		{
+			int width = 0;
+			setWidth(-20, &width);
+			Assert::AreEqual(0, width); //Negative (Invalid) input case
+		}
+
+		TEST_METHOD(TestSetWidth_MaximumValidInput)
+		{
+			int width = 0;
+			setWidth(99, &width);
+			Assert::AreEqual(99, width); //Upper edge case
+		}
+
 	};
 }
